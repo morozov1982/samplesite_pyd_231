9b14789f8a5d470149d5e52968590c060c6061bc
Морозов Александр <@step.kz>
2023-11-23 21:35:34 +0600

    lesson_06 files

--- bboard/models.py
+++ bboard/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 
+is_all_posts_passive = True
+
+
+def is_active_default():
+    return is_all_posts_passive
+
+
 class Rubric(models.Model):
-    name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Название', unique=True)
 
     def __str__(self):
         return self.name
@@ -14,12 +21,38 @@ class Rubric(models.Model):
 
 
 class Bb(models.Model):
+    # KINDS = (
+    #     ('b', 'Куплю'),
+    #     ('s', 'Продам'),
+    #     ('c', 'Обменяю'),
+    # )
+    # KINDS = (
+    #     ('Купля-продажа', (
+    #         ('b', 'Куплю'),
+    #         ('s', 'Продам'),
+    #      )),
+    #     ('Обмен', (
+    #         ('c', 'Обменяю'),
+    #     ))
+    # )
+    KINDS = (
+        (None, 'Выберите тип объявления'),
+        ('b', 'Куплю'),
+        ('s', 'Продам'),
+        ('c', 'Обменяю'),
+    )
+
+    # kind = models.CharField(max_length=1, choices=KINDS, default='s')
+    kind = models.CharField(max_length=1, choices=KINDS, blank=True)
+
     rubric = models.ForeignKey("Rubric", null=True, on_delete=models.PROTECT,
                                verbose_name='Рубрика')
     title = models.CharField(max_length=50, verbose_name="Товар")
     content = models.TextField(null=True, blank=True, verbose_name="Описание")
-    price = models.FloatField(null=True, blank=True, verbose_name="Цена")
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Цена", default=0)
+    is_active = models.BooleanField(default=is_active_default)
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name="Опубликовано")
+    updated = models.DateTimeField(auto_now=True, db_index=True, verbose_name="Изменено")
 
     def __str__(self):
         return f'{self.title}'
@@ -27,4 +60,4 @@ class Bb(models.Model):
     class Meta:
         verbose_name_plural = 'Объявления'
         verbose_name = 'Объявление'
-        ordering = ['-published']
+        ordering = ['-published', 'title']
