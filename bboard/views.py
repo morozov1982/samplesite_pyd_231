469f69880a8716b1e930ecc45488e067011ba73b
Морозов Александр <@step.kz>
2023-12-19 20:40:11 +0600

    lesson_13 files

--- bboard/views.py
+++ bboard/views.py
@@ -32,7 +32,7 @@ def by_rubric(request, rubric_id):
 class BbCreateView(CreateView):
     template_name = 'create.html'
     form_class = BbForm
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('bboard:index')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
