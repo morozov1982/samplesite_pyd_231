5a47867e57332c5885e7c59c6588676c5d9c1cc1
Морозов Александр <@step.kz>
2024-01-09 21:07:54 +0600

    lesson_17 files

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,18 +1,14 @@
-from django.urls import path, re_path
+from django.urls import path
 
 from .views import (index, BbByRubricView,
-                    # by_rubric,
-                    BbCreateView, add_and_save)
+                    BbCreateView, BbDetailView)
 
 
 app_name = 'bboard'
 
 urlpatterns = [
-    # path('add/', add_and_save, name='add'),
-
+    path('detail/<int:pk>/', BbDetailView.as_view(), name='detail'),
     path('add/', BbCreateView.as_view(), name='add'),
-
-    # path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('<int:rubric_id>/', BbByRubricView.as_view(), name='by_rubric'),
 
     path('', index, name='index'),
--- bboard/views.py
+++ bboard/views.py
@@ -1,9 +1,9 @@
 from django.db.models import Count
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect, HttpResponseNotFound, Http404
-from django.template import loader
-from django.template.loader import get_template, render_to_string
-from django.urls import reverse_lazy, reverse
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 
@@ -20,49 +20,39 @@ def index(request):
     )
 
 
-# def by_rubric(request, rubric_id):
-#     bbs = Bb.objects.filter(rubric=rubric_id)
-#     rubrics = Rubric.objects.annotate(cnt=Count('bb')).filter(cnt__gt=0)
-#     current_rubric = Rubric.objects.get(pk=rubric_id)
-#     context = {'bbs': bbs, 'rubrics': rubrics,
-#                'current_rubric': current_rubric}
-#     return render(request, 'by_rubric.html', context)
-
-
-class BbByRubricView(TemplateView):
+class BbByRubricView(ListView):
     template_name = 'by_rubric.html'
+    context_object_name = 'bbs'
+
+    def get_queryset(self):
+        return Bb.objects.filter(rubric=self.kwargs['rubric_id'])
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['bbs'] = Bb.objects.filter(rubric=context['rubric_id'])
         context['rubrics'] = Rubric.objects.all()
-        context['current_rubric'] = Rubric.objects.get(pk=context['rubric_id'])
+        context['current_rubric'] = Rubric.objects.get(
+            pk=self.kwargs['rubric_id'])
         return context
 
 
-# def add_and_save(request):
-#     print(request.headers['Accept-Encoding'])
-#     print(request.headers['accept-encoding'])
-#     print(request.headers['accept_encoding'])
-#     print(request.headers['Cookie'])
-#     print(request.resolver_match)
-#     print(request.body)
+# class BbByRubricView(TemplateView):
+#     template_name = 'by_rubric.html'
 #
-#     if request.method == 'POST':
-#         bbf = BbForm(request.POST)
-#         if bbf.is_valid():
-#             bbf.save()
-#             return HttpResponseRedirect(
-#                 reverse('bboard:by_rubric',
-#                         kwargs={'rubric_id': bbf.cleaned_data['rubric'].pk})
-#             )
-#         else:
-#             context = {'form': bbf}
-#             return render(request, 'create.html', context)
-#     else:
-#         bbf = BbForm()
-#         context = {'form': bbf}
-#         return render(request, 'create.html', context)
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['bbs'] = Bb.objects.filter(rubric=context['rubric_id'])
+#         context['rubrics'] = Rubric.objects.all()
+#         context['current_rubric'] = Rubric.objects.get(pk=context['rubric_id'])
+#         return context
+
+
+class BbDetailView(DetailView):
+    model = Bb
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
 
 
 class BbCreateView(CreateView):
